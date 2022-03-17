bfb843de5cefa9a0f6e04ec2c2bde719f2fbf96b
Jelai Wang <14236583+jelaiw@users.noreply.github.com>
2022-03-17 16:12:14 +0000

    Add readAuditLog() to audit controller.

--- src/main/java/com/manning/apisecurityinaction/controller/AuditController.java
+++ src/main/java/com/manning/apisecurityinaction/controller/AuditController.java
@@ -2,14 +2,37 @@ package com.manning.apisecurityinaction.controller;
 
 import org.dalesbred.Database;
 import org.json.JSONObject;
+import org.json.JSONArray;
 import spark.Request;
 import spark.Response;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 public class AuditController {
 	private final Database database;
 
 	public AuditController(Database database) {
 		this.database = database;
+	}
+
+	public JSONArray readAuditLog(Request request, Response response) {
+		var since = Instant.now().minus(1, ChronoUnit.HOURS);
+		var logs = database.findAll(AuditController::recordToJson,
+			"SELECT * FROM audit_log WHERE audit_time >= ? LIMIT 20", since);
+		return new JSONArray(logs);
+	}
+
+	private static JSONObject recordToJson(ResultSet row) throws SQLException {
+		return new JSONObject()
+			.put("id", row.getLong("audit_id"))
+			.put("method", row.getString("method"))
+			.put("path", row.getString("path"))
+			.put("status", row.getInt("status"))
+			.put("user", row.getString("user_id"))
+			.put("time", row.getTimestamp("audit_time").toInstant());
 	}
 
 	public void auditRequestStart(Request request, Response response) {
