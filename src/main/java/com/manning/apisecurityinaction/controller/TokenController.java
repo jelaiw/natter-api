1bb3cf11a808ff1e62a848ecc6c982470a62aaab
Jelai Wang <14236583+jelaiw@users.noreply.github.com>
2022-04-07 15:01:47 +0000

    Add first pass at TokenController class.

---
+++ src/main/java/com/manning/apisecurityinaction/controller/TokenController.java
@@ -0,0 +1,29 @@
+package com.manning.apisecurityinaction.controller;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+import org.json.JSONObject;
+import spark.Request;
+import spark.Response;
+import com.manning.apisecurityinaction.token.TokenStore;
+
+public class TokenController {
+	private final TokenStore tokenStore;
+
+	public TokenController(TokenStore tokenStore) {
+		this.tokenStore = tokenStore;
+	}
+
+	public JSONObject login(Request request, Response response) {
+		String subject = request.attribute("subject");
+		// See https://mkyong.com/java/java-how-to-get-current-date-time-date-and-calender/.
+		var expiry = Instant.now().plus(8, ChronoUnit.MINUTES);
+
+		var token = new TokenStore.Token(expiry, subject);
+		var tokenId = tokenStore.create(request, token);
+
+		response.status(201);
+		return new JSONObject().put("token", tokenId);
+	}
+}
