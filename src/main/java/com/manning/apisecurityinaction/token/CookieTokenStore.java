16d6e0e3657c8d4abff477ef198ca5079bde2878
Jelai Wang <14236583+jelaiw@users.noreply.github.com>
2022-04-08 14:33:59 +0000

    Add cookie-based TokenStore implementation.

---
+++ src/main/java/com/manning/apisecurityinaction/token/CookieTokenStore.java
@@ -0,0 +1,31 @@
+package com.manning.apisecurityinaction.token;
+
+import java.util.Optional;
+import spark.Request;
+
+public class CookieTokenStore implements TokenStore {
+	@Override
+	public String create(Request request, Token token) {
+		// WARNING: Vulnerable to session fixation attack.
+		var session = request.session(true);
+
+		session.attribute("username", token.username);
+		session.attribute("expiry", token.expiry);
+		session.attribute("attrs", token.attributes);
+
+		return session.id();
+	}
+
+	@Override
+	public Optional<Token> read(Request request, String tokenId) {
+		var session = request.session(false);
+		if (session == null) {
+			return Optional.empty();
+		}
+
+		var token = new Token(session.attribute("expiry"), session.attribute("username"));
+		token.attributes.putAll(session.attribute("attrs"));
+
+		return Optional.of(token);
+	}
+}
