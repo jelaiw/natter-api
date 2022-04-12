f025b552ebf35d2e7ebc3e9c865ea395d0a01e1c
Jelai Wang <14236583+jelaiw@users.noreply.github.com>
2022-04-12 18:14:04 +0000

    Test full cookie-based session authentication.
    Update curl calls to use cookie jar.

--- src/test/resources/com/manning/apisecurityinaction/controller/test-sessions.sh
+++ src/test/resources/com/manning/apisecurityinaction/controller/test-sessions.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 echo "Register demo user."
 curl -i -d @register_demo_user.json -H 'Content-Type: application/json' https://localhost:4567/users
-echo "\nEstablish a session."
-curl -u demo:password -i -H 'Content-Type: application/json' -X POST https://localhost:4567/sessions
+echo "\nEstablish a session (cookie-based)."
+#curl -u demo:password -i -H 'Content-Type: application/json' -X POST https://localhost:4567/sessions
+curl -c /tmp/cookies -u demo:password -i -H 'Content-Type: application/json' -X POST https://localhost:4567/sessions
+echo "\nPeek at contents of cookie jar."
+cat /tmp/cookies
+echo "\nCreate test space (as part of existing demo user session)."
+curl -b /tmp/cookies -i -d @create_space.json -H 'Content-Type: application/json' https://localhost:4567/spaces
+echo "\nRemove cookie jar for cleanup."
+rm /tmp/cookies
 #curl https://localhost:4567/logs | jq
