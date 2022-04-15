7960fcf0043bd5e9d0063963d2348d1af6404601
Jelai Wang <14236583+jelaiw@users.noreply.github.com>
2022-04-15 15:53:27 +0000

    Add first pass at Natter login UI.

---
+++ src/main/resources/public/login.js
@@ -0,0 +1,37 @@
+const apiUrl = 'https://localhost:4567';
+
+function login(username, password) {
+	// See https://developer.mozilla.org/en-US/docs/Web/API/btoa.
+	let credentials = 'Basic ' + btoa(username + ":" + password);
+
+	fetch(apiUrl + '/sessions', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': credentials
+		}
+	})
+	.then(response => {
+		if (response.ok) {
+			response.json().then(json => {
+				document.cookie = 'csrfToken=' + json.token + ';Secure;SameSite=strict';
+				window.location.replace('/natter.html');
+			});
+		}
+	})
+	.catch(error => console.error('Error logging in: ', error));
+}
+
+window.addEventListener('load', function(e) {
+	document.getElementById('login').addEventListener('submit', processLoginSubmit);
+});
+
+function processLoginSubmit(e) {
+	e.preventDefault();
+
+	let username = document.getElementById('username').value;
+	let password = document.getElementById('password').value;
+	login(username, password);
+
+	return false;
+}
