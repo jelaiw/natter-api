824949a9de1165d9a35d172e745fda9b85d6703a
Jelai Wang <14236583+jelaiw@users.noreply.github.com>
2022-04-15 16:26:29 +0000

    Update header with CSRF token in create space call.
    Add getCookie() helper method.

--- src/main/resources/public/natter.js
+++ src/main/resources/public/natter.js
@@ -2,18 +2,23 @@ const apiUrl = 'https://localhost:4567';
 
 function createSpace(name, owner) {
 	let data = {name: name, owner: owner};
+	let csrfToken = getCookie('csrfToken');
 
 	fetch(apiUrl + '/spaces', {
 		method: 'POST',
 		credentials: 'include',
 		body: JSON.stringify(data),
 		headers: {
-			'Content-Type': 'application/json'
+			'Content-Type': 'application/json',
+			'X-CSRF-Token': csrfToken
 		}
 	})
 	.then(response => {
 		if (response.ok) {
 			return response.json();
+		}
+		else if (response.status === 401) {
+			window.location.replace('/login.html');
 		}
 		else {
 			throw Error(response.statusText);
@@ -21,6 +26,16 @@ function createSpace(name, owner) {
 	})
 	.then(json => console.log('Created space: ', json.name, json.url))
 	.catch(error => console.error('Error: ', error));
+}
+
+function getCookie(cookieName) {
+	var cookieValue = document.cookie.split(';')
+		.map(item => item.split('=').map(x => decodeURIComponent(x.trim())))
+		.filter(item => item[0] === cookieName)[0]
+
+	if (cookieValue) {
+		return cookieValue[1];
+	}
 }
 
 window.addEventListener('load', function(e) {
