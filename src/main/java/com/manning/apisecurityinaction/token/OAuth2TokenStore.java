d7c3dd18db373492262228e48c94c3eaf86974a1
Jelai Wang <14236583+jelaiw@users.noreply.github.com>
2022-05-24 14:52:19 +0000

    Secure HTTPS connection per chapter 7.4.2.

--- src/main/java/com/manning/apisecurityinaction/token/OAuth2TokenStore.java
+++ src/main/java/com/manning/apisecurityinaction/token/OAuth2TokenStore.java
@@ -9,8 +9,14 @@ import java.net.URLEncoder;
 import java.util.Base64;
 import java.util.Optional;
 import java.io.IOException;
+import java.io.FileInputStream;
 import java.time.Instant;
 import static java.nio.charset.StandardCharsets.UTF_8;
+import javax.net.ssl.SSLParameters;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.SSLContext;
+import java.security.KeyStore;
+import java.security.GeneralSecurityException;
 
 import spark.Request;
 import org.json.JSONObject;
@@ -28,7 +34,39 @@ public class OAuth2TokenStore implements SecureTokenStore {
 		var credentials = URLEncoder.encode(clientId, UTF_8) + ":" + URLEncoder.encode(clientSecret, UTF_8);
 		this.authorization = "Basic " + Base64.getEncoder().encodeToString(credentials.getBytes(UTF_8));
 
-		this.httpClient = HttpClient.newHttpClient();
+		var sslParams = new SSLParameters();
+		// See Mozilla "intermediate" configuration at https://wiki.mozilla.org/Security/Server_Side_TLS.
+		sslParams.setProtocols(new String[] { "TLSv1.3", "TLSv1.2" });
+		sslParams.setCipherSuites(new String[] {
+	        "TLS_AES_128_GCM_SHA256",
+	        "TLS_AES_256_GCM_SHA384",
+	        "TLS_CHACHA20_POLY1305_SHA256",
+	        "TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
+	        "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
+	        "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384",
+	        "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
+	        "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256",
+	        "TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256"
+		});
+		sslParams.setUseCipherSuitesOrder(true);
+		sslParams.setEndpointIdentificationAlgorithm("HTTPS");
+
+		try {
+			var trustedCerts = KeyStore.getInstance("PKCS12");
+			trustedCerts.load(new FileInputStream("as.example.com.ca.p12"), "changeit".toCharArray());
+			var tmf = TrustManagerFactory.getInstance("PKIX");
+			tmf.init(trustedCerts);
+			var sslContext = SSLContext.getInstance("TLS");
+			sslContext.init(null, tmf.getTrustManagers(), null);
+
+			this.httpClient = HttpClient.newBuilder()
+				.sslParameters(sslParams)
+				.sslContext(sslContext)
+				.build();
+		}
+		catch (GeneralSecurityException | IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	@Override
