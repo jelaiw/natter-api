b3c96652ee16dbae58a4147d17e85591145e4ee9
Jelai Wang <14236583+jelaiw@users.noreply.github.com>
2022-06-17 14:55:14 +0000

    Add CapabilityController.share().
    Need a new mechanism for sharing access to social spaces and messages (after capability hardening exercise).

--- src/main/java/com/manning/apisecurityinaction/controller/CapabilityController.java
+++ src/main/java/com/manning/apisecurityinaction/controller/CapabilityController.java
@@ -10,6 +10,8 @@ import java.util.Objects;
 
 import spark.Request;
 import spark.Response;
+import static spark.Spark.halt;
+import org.json.JSONObject;
 
 public class CapabilityController {
 	private final SecureTokenStore tokenStore;
@@ -49,5 +51,40 @@ public class CapabilityController {
 				request.attribute("perms", token.attributes.get("perms"));
 			}
 		});
+	}
+
+	public JSONObject share(Request request, Response response) {
+		var json = new JSONObject(request.body());
+
+		// Parse original capability URI for token.
+		var capUri = URI.create(json.getString("uri"));
+		var path = capUri.getPath();
+		var query = capUri.getQuery();
+		var tokenId = query.substring(query.indexOf('=') + 1);
+
+		// Look up token and check that the resource path matches.
+		var token = tokenStore.read(request, tokenId).orElseThrow();
+		if (!Objects.equals(token.attributes.get("path"), path)) {
+			throw new IllegalArgumentException("incorrect path");
+		}
+
+		// Check that requested permissions are a subset of the token perms.
+		var tokenPerms = token.attributes.get("perms");
+		var perms = json.optString("perms", tokenPerms);
+		if (!tokenPerms.contains(perms)) {
+			halt(403);
+		}
+
+		// Create and store new capability token (for intended user).
+		var user = json.getString("user");
+		var newToken = new Token(token.expiry, user);
+		newToken.attributes.put("path", path);
+		newToken.attributes.put("perms", perms);
+		var newTokenId = tokenStore.create(request, newToken);
+
+		// Return requested capability URI.
+		var uri = URI.create(request.uri());
+		var newCapUri = uri.resolve(path + "?access_token=" + newTokenId);
+		return new JSONObject().put("uri", newCapUri);
 	}
 }
