abc4d320073b6c4447c359ac63ee4286c54102e5
Jelai Wang <14236583+jelaiw@users.noreply.github.com>
2022-06-22 14:21:16 +0000

    Add SinceVerifier for first-party caveat exercise.

--- src/main/java/com/manning/apisecurityinaction/token/MacaroonTokenStore.java
+++ src/main/java/com/manning/apisecurityinaction/token/MacaroonTokenStore.java
@@ -2,10 +2,13 @@ package com.manning.apisecurityinaction.token;
 
 import java.util.Optional;
 import java.security.Key;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 import com.github.nitram509.jmacaroons.MacaroonsBuilder;
 import com.github.nitram509.jmacaroons.MacaroonsVerifier;
 import com.github.nitram509.jmacaroons.verifier.TimestampCaveatVerifier;
+import com.github.nitram509.jmacaroons.GeneralCaveatVerifier;
 import spark.Request;
 
 public class MacaroonTokenStore implements SecureTokenStore {
@@ -42,6 +45,7 @@ public class MacaroonTokenStore implements SecureTokenStore {
 		var verifier = new MacaroonsVerifier(macaroon);
 		verifier.satisfyGeneral(new TimestampCaveatVerifier());
 		verifier.satisfyExact("method = " + request.requestMethod());
+		verifier.satisfyGeneral(new SinceVerifier(request));
 		if (verifier.isValid(macKey.getEncoded())) {
 			return delegate.read(request, macaroon.identifier);
 		}
@@ -52,5 +56,31 @@ public class MacaroonTokenStore implements SecureTokenStore {
 	public void revoke(Request request, String tokenId) {
 		var macaroon = MacaroonsBuilder.deserialize(tokenId);
 		delegate.revoke(request, macaroon.identifier);
+	}
+
+	private static class SinceVerifier implements GeneralCaveatVerifier {
+		private final Request request;
+
+		private SinceVerifier(Request request) {
+			this.request = request;
+		}
+
+		@Override
+		public boolean verifyCaveat(String caveat) {
+			if (caveat.startsWith("since > ")) {
+				// Parse restriction if caveat matches.
+				var minSince = Instant.parse(caveat.substring(8));
+
+				// Determine "since" parameter value on the request.
+				var reqSince = Instant.now().minus(1, ChronoUnit.DAYS);
+				if (request.queryParams("since") != null) {
+					reqSince = Instant.parse(request.queryParams("since"));
+				}
+				// Satisfy caveat if the request is after the earliest message restriction.
+				return reqSince.isAfter(minSince);
+			}
+			// Reject all other caveats.
+			return false;
+		}
 	}
 }
