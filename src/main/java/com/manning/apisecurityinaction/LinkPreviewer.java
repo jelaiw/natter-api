fd3c4aa39dfd1a97f990b8c37d91da3928183891
Jelai Wang <14236583+jelaiw@users.noreply.github.com>
2022-07-11 13:41:14 +0000

    Implement blocklist for link preview service.

--- src/main/java/com/manning/apisecurityinaction/LinkPreviewer.java
+++ src/main/java/com/manning/apisecurityinaction/LinkPreviewer.java
@@ -10,9 +10,28 @@ import org.jsoup.Jsoup;
 import org.json.JSONObject;
 import java.net.MalformedURLException;
 import java.net.UnknownHostException;
+import java.net.URI;
+import java.net.InetAddress;
+import java.net.Inet6Address;
 
 public class LinkPreviewer {
 	private static final Logger logger = LoggerFactory.getLogger(LinkPreviewer.class);
+
+	private static boolean isBlockedAddress(String uri) throws UnknownHostException {
+		var host = URI.create(uri).getHost();
+		for (var ipAddr : InetAddress.getAllByName(host)) {
+			if (ipAddr.isLoopbackAddress() || ipAddr.isLinkLocalAddress() || ipAddr.isSiteLocalAddress() || ipAddr.isMulticastAddress() || ipAddr.isAnyLocalAddress() || isUniqueLocalAddress(ipAddr)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private static boolean isUniqueLocalAddress(InetAddress ipAddr) {
+		return ipAddr instanceof Inet6Address
+			&& (ipAddr.getAddress()[0] & 0xFF) == 0xFD
+			&& (ipAddr.getAddress()[1] & 0xFF) == 0x00;
+	}
 
 	public static void main(String...args) {
 		afterAfter((request, response) -> {
@@ -20,6 +39,9 @@ public class LinkPreviewer {
 		});
 		get("/preview", (request, response) -> {
 			var url = request.queryParams("url");
+			if (isBlockedAddress(url)) {
+				throw new IllegalArgumentException("URL refers to local/private address");
+			}
 			var doc = Jsoup.connect(url).timeout(2999).get();
 			// Extract desired metadata properties from HTML.
 			var title = doc.title();
