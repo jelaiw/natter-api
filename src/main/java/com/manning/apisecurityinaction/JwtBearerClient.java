b2a58c36527b4609e5e588e7f0b483af6b14aa11
Jelai Wang <14236583+jelaiw@users.noreply.github.com>
2022-07-20 14:34:34 +0000

    Generate JWT (to be used as a client assertion).

--- src/main/java/com/manning/apisecurityinaction/JwtBearerClient.java
+++ src/main/java/com/manning/apisecurityinaction/JwtBearerClient.java
@@ -3,8 +3,17 @@ package com.manning.apisecurityinaction;
 import java.io.FileInputStream;
 import java.security.KeyStore;
 import java.security.interfaces.ECPrivateKey;
+import java.util.Date;
+import java.util.UUID;
+import static java.time.Instant.now;
+import static java.time.temporal.ChronoUnit.SECONDS;
 
 import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import com.nimbusds.jose.crypto.ECDSASigner;
 import static spark.Spark.secure;
 import static spark.Spark.get;
 
@@ -24,5 +33,20 @@ public class JwtBearerClient {
 			response.type("application/jwk-set+json");
 			return jwkSet.toString();
 		});
+
+		// See chapter 11.3.2 for further context.
+		var clientId = "test";
+		var as = "https://as.example.com:8080/oauth2/access_token";
+		var header = new JWSHeader.Builder(JWSAlgorithm.ES256).keyID("es256-key").build();
+		var claims = new JWTClaimsSet.Builder()
+				.subject(clientId)
+				.issuer(clientId)
+				.expirationTime(Date.from(now().plus(30, SECONDS)))
+				.audience(as)
+				.jwtID(UUID.randomUUID().toString()) // Add a random JWT ID claim to prevent replay.
+				.build();
+		var jwt = new SignedJWT(header, claims);
+		jwt.sign(new ECDSASigner(privateKey)); // Sign JWT with private key.
+		var assertion = jwt.serialize();
 	}
 }
